3ab389cab7f38bdc79cd7b3744f0824dba232c0d
khcho902 <khcho902@gmail.com>
2023-02-15 22:02:20 +0900

    init project

---
+++ src/main/kotlin/com/kycho/couponsystem/CouponSystemApplication.kt
@@ -0,0 +1,11 @@
+package com.kycho.couponsystem
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CouponSystemApplication
+
+fun main(args: Array<String>) {
+	runApplication<CouponSystemApplication>(*args)
+}
